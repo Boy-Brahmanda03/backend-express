a76eebc39b8d6f9e838693f03c595812345420f8
Boy-Brahmanda03 <boybrahmanda03@gmail.com>
2024-11-14 01:35:00 +0800

    connect with no env on others

--- database.js
+++ database.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 const mysql = require("mysql");
 const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
+	host: "ppl2024-backend-ppls.d.aivencloud.com",
+	user: "avnadmin",
 	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	port: process.env.PORT,
+	database: "defaultdb",
+	port: 20860,
 	ssl: {
 		rejectUnauthorized: true,
 		ca: fs.readFileSync("ca.pem"), // Path to the CA certificate file
