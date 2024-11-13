1f037ea776b608715902384c5adc02ea0ffb3507
Boy-Brahmanda03 <boybrahmanda03@gmail.com>
2024-11-14 01:26:53 +0800

    db connect

--- database.js
+++ database.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 
 const mysql = require("mysql");
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.PORT,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync("ca.pem"), // Path to the CA certificate file
-  },
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	port: process.env.PORT,
+	ssl: {
+		rejectUnauthorized: true,
+		ca: fs.readFileSync("ca.pem"), // Path to the CA certificate file
+	},
 });
 
 module.exports = db;
